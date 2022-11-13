28cdcb762a270821a3f2f2278f4f62619a0ff101
Shivram <sr31415@outlook.com>
2022-11-13 18:14:48 -0500

    Create and style taskAdder

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, SafeAreaView, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 import Task from './components/Task.js';
 
 export default function App() {
@@ -12,6 +12,16 @@ export default function App() {
           <Task>Another task</Task>
         </View>
       </SafeAreaView>
+			<KeyboardAvoidingView
+				behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+				style={styles.taskWriter}>
+					<TextInput style={styles.textinput} placeholder='Add task' />
+          <TouchableOpacity>
+            <View style={styles.addButton}>
+              <Text style={styles.plus}>+</Text>
+            </View>
+          </TouchableOpacity>
+			</KeyboardAvoidingView>
     </View>
   );
 }
@@ -31,5 +41,35 @@ const styles = StyleSheet.create({
   },
   taskList: {
     marginTop: 20,
+  },  
+  taskWriter: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  addButton: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  textinput: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  plus: {
+    fontSize: 30,
   }
 });
